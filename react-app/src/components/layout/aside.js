a3c6dd6e94191230bb30ddff7040f8e810d3fe64
afrkncgds <frkncagdas@gmail.com>
2025-03-10 00:15:18 +0300

    updated

--- react-app/src/components/layout/aside.js
+++ react-app/src/components/layout/aside.js
@@ -3,37 +3,40 @@ import React from 'react';
 function Aside() {
     return (
         <div className="aside-root">
-            <div className="logo-block">
+         <div className='top-block'>
+         <div className="logo-block">
 
-                <a href="/" className="logo-item">
-                    <img src="img/cv-foto.png" alt="logo" />
-                </a>
-            </div>
-            <div className="name-block">
-                <p>
-                    AYHAN FURKAN ÇAĞDAŞ
-                </p>
-            </div>
-            <div className="menu-block">
-                <ul>
-                    <li>
-                        <a href="/">Hakkımda</a>
-                    </li>
-                    <li>
-                        <a href="/">Yetenekler</a>
-                    </li>
-                    <li>
-                        <a href="/">Portfolyo</a>
-                    </li>
-                    <li>
-                        <a href="/">Blog</a>
-                    </li>
-                    <li>
-                        <a href="/">İletişim</a>
-                    </li>
+<a href="/" className="logo-item">
+    <img src="img/cv-foto.png" alt="logo" />
+</a>
+</div>
+<div className="name-block">
+<p>
+    AYHAN FURKAN ÇAĞDAŞ
+</p>
+</div>
+<div className="menu-block">
+<ul>
+    <li>
+        <a href="/">Hakkımda</a>
+    </li>
+    <li>
+        <a href="/">Yetenekler</a>
+    </li>
+    <li>
+        <a href="/">Portfolyo</a>
+    </li>
+    <li>
+        <a href="/">Blog</a>
+    </li>
+    <li>
+        <a href="/">İletişim</a>
+    </li>
 
-                </ul>
-            </div>  
+</ul>
+</div>  
+         </div>
+        
             <div className="social-block">
                 <ul>
                     <li>
@@ -48,7 +51,10 @@ function Aside() {
             </div>
             <div className="copyright-block">
                 <p>
-                    Copyright © 2025 Ayhan Furkan Çağdaş. Tüm hakları saklıdır.
+                    Copyright © 2025 Ayhan Furkan Çağdaş.
+                </p>
+                <p>
+                Tüm hakları saklıdır.
                 </p>
             </div>
         </div>
--- react-app/src/pages/homePage.js
+++ react-app/src/pages/homePage.js
@@ -6,12 +6,44 @@ function HomePage() {
         <div className="home-root">
             <Aside />
             <div className="home-wrapper">
+                <div className='home-container'>
                 <div className="image-wrapper">
                     <img src="img/cv-foto.png" alt="profile" />
                 </div>
                 <div className="text-wrapper">
-                    <h1>Ayhan Furkan Çağdaş</h1>
-                    <p>Frontend Developer</p>
+                    <div className='title-block'>
+                        <p>
+                            Front-end Develooper
+                        </p>
+                        <h1>
+                            Ayhan Furkan Çağdaş
+                        </h1>
+                    </div>
+                    <div className='text-block'>
+                        <div className='text-item'>
+                            <p>
+                            Uzun yıllar yönetici konumunda iş
+deneyimi olan, ekip çalışmasına
+yatkın, ikili ilişkileri iyi, temiz ve kısa
+kod yazılımına önem veren,
+sorumluluk bilinci olan, stres
+yönetiminde başarılı, yenilikleri
+takip eden ve uyum sağlayan mid
+level full-stack developer ve project
+manager assistanım. Teknik
+doküman okuyup takip edebilecek
+kadar İngilizce bilgisine sahibim.
+                            </p>
+                        </div>
+                        <div className='button-block'>
+                            <div className='btn-item'>
+                                <span>CV İndir</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    
+                </div>
                 </div>
             </div>
         </div>
